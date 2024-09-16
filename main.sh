5bfba06a05b66762e9d549a0a9abc22fc04e4df5
lekseng <isaisa8784@gmail.com>
2024-09-17 01:21:26 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -278,7 +278,7 @@ TEXT="
 <code>Exp Sc : </code><code>$EXPSC</code>
 <code>────────────────────</code>
 <i>Automatic Notification from Github</i>
-"'&reply_markup={"inline_keyboard":[[{"text":"ᴏʀᴅᴇʀ","url":"https://t.me/@Jms_Store"},{"text":"Contack","url":"https://wa.me/6285872036588"}]]}'
+"'&reply_markup={"inline_keyboard":[[{"text":"ᴏʀᴅᴇʀ","url":"https://t.me/Jms_Store"},{"text":"Contack","url":"https://wa.me/6282288325977"}]]}'
 curl -s --max-time $TIMES -d "chat_id=$CHATID&disable_web_page_preview=1&text=$TEXT&parse_mode=html" $URL >/dev/null
 }
 clear
